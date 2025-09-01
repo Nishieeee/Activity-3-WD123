00016e715b94815523e4fd0b9e172f3eb2d040ce
Cleeein <pagaroganjhonclein@gmail.com>
2025-09-01 20:51:08 +0800

    add: 4. input sanitation

--- viewProduct.php
+++ viewProduct.php
@@ -1,4 +1,4 @@
-<label for="">Product Name: <?php echo $_POST["product_name"]; ?></label><br>
+<label for="">Product Name: <?php echo htmlspecialchars($_POST["product_name"]); ?></label><br>
 <label for="">Category: <?php echo $_POST["category"]; ?></label><br>
 <label for="">Price: <?php echo number_format($_POST["price"], 2); ?></label><br>
 <label for="">Stock Quantity: <?php echo $_POST["stock_quantity"]; ?></label><br>
